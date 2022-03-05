7996ec25c6ba5622fe4914c6bd7eaf644560ed76
Emily <emilykiddell@gmail.com>
2022-03-05 21:29:41 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Mickey Mouse',
+    streetAddress: '1 Paradise Lane, Disneyland'
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newObj = { ...obj};
+    newObj[key] = value;
+    return newObj;
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(employee, 'country', 'United States');
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+const updatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'phone number', '(123) 456-7890');
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj};
+    key = delete newObj[key];
+    return newObj;
+}
+
+const cloneEmployee = deleteFromEmployeeByKey(employee, 'streetAddress');
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    key = delete employee[key];
+    return employee;
+}
+
+destructivelyDeleteFromEmployeeByKey(employee, 'phone number');
